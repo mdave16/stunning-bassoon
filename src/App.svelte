ff190c566d6896480ff3716536a88ec7c1ec6ee8
mdave16 <mdave3927@gmail.com>
2020-12-27 22:37:38 +0000

    adding data

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,9 @@
-<h1>Hello world!</h1>
+<style>
+	p {
+		color: purple;
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS',
+			sans-serif;
+	}
+</style>
+
+<p>This is a paragraph</p>
