8727f9abd808229238e449dd27fae672accf70ef
mdave16 <mdave3927@gmail.com>
2020-12-28 01:02:15 +0000

    spread operator and extra props

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script>
 	import Nested from './Nested.svelte';
+	const info = {
+		answer: 'working',
+		name: 'spread operator',
+	};
 </script>
 
 <Nested answer={423} />
-<Nested />
+<Nested {...info} />
+<Nested {...info} anotherThing="secret extra info" />
--- src/Nested.svelte
+++ src/Nested.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	export let answer = 'a mystery';
+	export let name = 'answer';
 </script>
 
-<p>The answer is {answer}</p>
+<p>The {name} is {answer}</p>
+<p>
+	Was I passed in extra props?
+	{Object.keys($$props).length > 2 ? 'Yes' : 'No'}
+</p>
