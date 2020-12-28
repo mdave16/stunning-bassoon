6fab9443844e99084674f762582d5c46d2595e9a
mdave16 <mdave3927@gmail.com>
2020-12-28 00:51:51 +0000

    declaring props

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,5 @@
-<script lang="ts">
-	let numbers = [1];
-	let addNumber = () => {
-		numbers[numbers.length] = numbers.length + 1;
-	};
-
-	$: sum = numbers.reduce((t, n) => t + n, 0);
+<script>
+	import Nested from './Nested.svelte';
 </script>
 
-<p>{numbers.join(' + ')} = {sum}</p>
-<button on:click={addNumber}> Add a number </button>
+<Nested answer={423} />
