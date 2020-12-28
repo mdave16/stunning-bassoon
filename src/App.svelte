66bc821229814b457f2a2869c99e4cd99296e676
mdave16 <mdave3927@gmail.com>
2020-12-28 00:32:48 +0000

    reactivity/declarations

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
 	let count = 0;
-
+	$: doubled = count * 2;
+	$: times = count === 1 ? 'time' : 'times';
 	const handleClick = () => {
 		count++;
 	};
 </script>
 
-<button on:click={handleClick}>
-	Clicked
-	{count}
-	{count === 1 ? 'time' : 'times'}
-</button>
+<button on:click={handleClick}> Clicked {count} {times} </button>
+<p>{count} doubled is {doubled}</p>
