81c940eda8cee63f8c8c654b2b9060c07f09505c
mdave16 <mdave3927@gmail.com>
2020-12-28 01:10:18 +0000

    using svelte else blocks

--- src/App.svelte
+++ src/App.svelte
@@ -5,5 +5,6 @@
 	};
 </script>
 
-{#if !user.loggedIn}<button on:click={toggle}>Log in</button>{/if}
-{#if user.loggedIn}<button on:click={toggle}>Log out</button>{/if}
+{#if !user.loggedIn}
+	<button on:click={toggle}>Log in</button>
+{:else}<button on:click={toggle}>Log out</button>{/if}
