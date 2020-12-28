51107a60974e32086bf44d105ddf3191ad3b2082
mdave16 <mdave3927@gmail.com>
2020-12-28 00:39:50 +0000

    reactivity/statements

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,11 @@
 	let count = 0;
 	$: doubled = count * 2;
 	$: times = count === 1 ? 'time' : 'times';
+	$: if (count >= 10) {
+		console.log(`the count is ${count}`);
+		count = 9;
+	}
+
 	const handleClick = () => {
 		count++;
 	};
