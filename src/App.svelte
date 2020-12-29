76808f84ed9f2e1bc4a1d46948fc2a2bf07b4f04
mdave16 <mdave3927@gmail.com>
2020-12-29 00:18:46 +0000

    using keyed each blocks

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
-	let cats = [
-		{ id: 'J---aiyznGQ', name: 'Keyboard Cat' },
-		{ id: 'z_AbfPXTKms', name: 'Maru' },
-		{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' },
+	import Thing from './Thing.svelte';
+
+	let things = [
+		{ id: 1, color: 'darkblue' },
+		{ id: 2, color: 'indigo' },
+		{ id: 3, color: 'deeppink' },
+		{ id: 4, color: 'salmon' },
+		{ id: 5, color: 'gold' },
 	];
+
+	const handleClick = () => {
+		console.log(things);
+		things = things.slice(1);
+		console.log(things);
+	};
 </script>
 
-<h1>Famous Cats of Youtube</h1>
-{#each cats as { id, name }, i}
-	<li>
-		<a target="_blank" href="https://youtube.com/watch?v={id}">
-			{i + 1}:
-			{name}
-		</a>
-	</li>
+<button on:click={handleClick}>Remove first thing </button>
+{#each things as thing (thing.id)}
+	<Thing current={thing.color} />
 {/each}
-<ul />
