df81d333f475c1b2c50780f131fd44491e64b26c
mdave16 <mdave3927@gmail.com>
2021-01-01 22:15:31 +0000

    svelte bindings take care of type coercion

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
-	let name = 'world';
+	let a = 1;
+	let b = 2;
 </script>
 
-<input bind:value={name} />
-<h1>Hello {name}!</h1>
+<label>
+	<input type="number" bind:value={a} min="0" max="10" />
+	<input type="range" bind:value={a} min="0" max="10" />
+</label>
+<br />
+<label>
+	<input type="number" bind:value={b} min="0" max="10" />
+	<input type="range" bind:value={b} min="0" max="10" />
+</label>
+<h1>{a} + {b} = {a + b}</h1>
