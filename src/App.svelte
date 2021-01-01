3c63f0b3d17a22ef27964d7a964e5024042a96c2
mdave16 <mdave3927@gmail.com>
2021-01-01 21:47:36 +0000

    use event modifiers to change how events are emitted

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,7 @@
 <script lang="ts">
-	let m = { x: 0, y: 0 };
-
-	const handleMouseMove = (event) => {
-		m.x = event.clientX;
-		m.y = event.clientY;
+	const handleClick = () => {
+		console.log('clicked');
 	};
 </script>
 
-<style>
-	div {
-		width: 100%;
-		height: 100%;
-	}
-</style>
-
-<div on:mousemove={handleMouseMove}>The mouse position is {m.x}, {m.y}</div>
+<button on:click|once={handleClick}> Click me </button>
