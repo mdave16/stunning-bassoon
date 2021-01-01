5c1c36f8eb67760d80ad1b419225754c8bda170c
mdave16 <mdave3927@gmail.com>
2021-01-01 22:46:40 +0000

    binding text areas and shorthand

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,13 @@
 <script lang="ts">
-	let scoops = 1;
-	let flavours = ['Mint choc chip'];
-
-	const menu = ['Cookies and cream', 'Mint choc chip', 'Raspberry ripple'];
-	const possibleScoops = [
-		{ number: 1, description: 'One' },
-		{ number: 2, description: 'Two' },
-		{ number: 3, description: 'Three' },
-	];
-	const join = (flavours: string[]) => {
-		if (flavours.length === 1) return flavours[0];
-		return `${flavours.slice(0, -1).join(', ')} and ${
-			flavours[flavours.length - 1]
-		}`;
-	};
+	let value = 'Some words are <em>italic</em> and some are <b>bold</b>';
 </script>
 
-<h2>size</h2>
-{#each possibleScoops as scoop}
-	<label>
-		<input type="radio" bind:group={scoops} value={scoop.number} />
-		{scoop.description}
-		{scoop.number === 1 ? 'scoop' : 'scoops'}
-	</label>
-{/each}
+<style>
+	textarea {
+		width: 100%;
+		height: 200px;
+	}
+</style>
 
-<h2>flavours</h2>
-
-{#each menu as flavour}
-	<label>
-		<input type="checkbox" bind:group={flavours} value={flavour} />
-		{flavour}
-	</label>
-{/each}
-
-{#if flavours.length === 0}
-	<p>Please select at least one flavour</p>
-{:else if flavours.length > scoops}
-	<p>Can't order more flavours than scoops!</p>
-{:else}
-	<p>
-		You ordered
-		{scoops}
-		{scoops === 1 ? 'scoop' : 'scoops'}
-		of
-		{join(flavours)}
-	</p>
-{/if}
+<textarea bind:value />
+{@html value}
