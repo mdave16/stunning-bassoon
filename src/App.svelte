3b960875d4b82297b1240db831b72799e6eb78a1
mdave16 <mdave3927@gmail.com>
2021-01-02 20:11:00 +0000

    add and remove points

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,17 @@
 	};
 	let points = [] as Point[];
 	const addPoint = (e) => {
-		points[points.length] = { x: e.x, y: e.y } as Point;
+		if (e.target.tagName === 'svg') {
+			points[points.length] = { x: e.clientX, y: e.clientY } as Point;
+		}
+	};
+
+	const removePoint = (e) => {
+		const p = {
+			x: parseInt(e.target.attributes.cx.value),
+			y: parseInt(e.target.attributes.cy.value),
+		} as Point;
+		points = points.filter((point) => point.x !== p.x && point.y !== p.y);
 	};
 </script>
 
@@ -20,15 +30,17 @@
 	}
 
 	circle {
-		color: black;
+		margin: -8px;
+		fill: orangered;
 	}
 </style>
 
 <div class="grid">
 	<svg on:click={addPoint}>
 		{#each points as p (p)}
-			<circle cx={p.x} cy={p.y} r={2} />
+			<circle cx={p.x} cy={p.y} r={5} on:click|preventDefault={removePoint} />
 		{/each}
+		<polygon points={points.map((p) => `${p.x}, ${p.y}`).join(' ')} />
 	</svg>
 
 	<div />
