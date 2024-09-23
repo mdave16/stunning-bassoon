5930b37617d071b74bd4dbfef4d467d7d50d8583
Mayur Dave <mayur.dave@dojo.tech>
2024-09-23 01:37:29 +0100

    Implement reading Eases

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,58 @@
 <script lang="ts">
-	type Point = {
-		x: number;
-		y: number;
+	let inputText = '';
+	const gradeToReadingEase = (grade: number): number => {
+		return Math.min(100, Math.max(0, 100 - ((grade - 1) / 19) * 100));
 	};
-	let points = [] as Point[];
-	const addPoint = (e) => {
-		if (e.target.tagName === 'svg') {
-			points[points.length] = { x: e.clientX, y: e.clientY } as Point;
-		}
+	const roundToNdp = (num: number, dp: number): number => {
+		const factor = Math.pow(10, dp);
+		return Math.round((num + Number.EPSILON) * factor) / factor;
 	};
-
-	const removePoint = (e) => {
-		const p = {
-			x: parseInt(e.target.attributes.cx.value),
-			y: parseInt(e.target.attributes.cy.value),
-		} as Point;
-		points = points.filter((point) => point.x !== p.x && point.y !== p.y);
+	const ariReadingEase = (text: string): number => {
+		const words = text.split(' ').length;
+		const characters = text.length;
+		const sentences = text.split(/[.!?]/).length;
+		const ari = 4.71 * (characters / words) + 0.5 * (words / sentences) - 21.43;
+		const readingEaseBounded = gradeToReadingEase(ari);
+		return roundToNdp(readingEaseBounded, 2);
 	};
 </script>
 
 <style>
 	.grid {
-		display: flex;
-		height: 100rem;
+		display: grid;
+		gap: 10px;
+		padding: 20px;
 	}
 
-	.grid * {
-		flex: 50%;
+	h1 {
+		font-size: 24px;
+		margin-bottom: 10px;
 	}
 
-	circle {
-		margin: -8px;
-		fill: orangered;
+	textarea {
+		height: 200px;
+		padding: 10px;
+		font-size: 16px;
+		margin: 10px 0;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	}
+
+	p {
+		font-size: 16px;
+		margin: 5px 0;
 	}
 </style>
 
 <div class="grid">
-	<svg on:click={addPoint}>
-		{#each points as p (p)}
-			<circle cx={p.x} cy={p.y} r={5} on:click|preventDefault={removePoint} />
-		{/each}
-		<polygon points={points.map((p) => `${p.x}, ${p.y}`).join(' ')} />
-	</svg>
+	<h1>Text Analysis</h1>
+	<p>Reading Ease from 0 (hard) to 100 (easy)</p>
+	<p>Automated Readability Index Reading Ease: {ariReadingEase(inputText)}</p>
+	<!-- <p>Gunning Fog Reading Ease: {ariReadingEase(inputText)}</p>
+	<p>Coleman-Liau Index Reading Ease: {ariReadingEase(inputText)}</p>
+	<p>Läsbarhetsindex Reading Ease: {ariReadingEase(inputText)}</p>
+	<p>Flesch-Kincaid Reading Ease: {ariReadingEase(inputText)}</p> -->
 
-	<div />
+	<textarea bind:value={inputText} placeholder="Text" />
 </div>
